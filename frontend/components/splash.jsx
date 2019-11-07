fca9193fc4cef97b24d42a0c3ca01a1a0fde8334
Joanna Chen <x.chenj@gmail.com>
2019-11-07 10:56:11 -0800

    Center splash page images and fix user login upon signup

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 const Splash = () => (
   <main className="splash">
     <div className="splash-image-container">
-      <div className="splash-items-left">
+
+      <div className="splash-items left">
         <img src="./ku.jpg" className="pic-left" alt=""/>
       </div>
-      <div className="splash-items-right">
+
+      <div className="splash-items right">
         <img src="./robertakeikokitaharasantana.jpg" className="pics-right" alt=""/>
         <img src="./danielvogel.jpg" className="pics-right" alt=""/>
       </div>
+      
     </div>
   </main>
 )
