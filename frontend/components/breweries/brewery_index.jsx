96a8da9571cb38397389ffbf15ce434dfef74377
Joanna Chen <x.chenj@gmail.com>
2019-12-09 16:52:13 -0800

    Create brewery index scroll into view

--- frontend/components/breweries/brewery_index.jsx
+++ frontend/components/breweries/brewery_index.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import Brewery from './brewery_index_item';
-import Map from '../map/map';
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
 
 class BreweryIndex extends React.Component {
   componentDidMount() {
-    this.props.fetchBreweries();
+    this.props.fetchBreweries()
+      .then(() => {
+        alphabet.forEach(alpha => {
+          document.getElementById(alpha).addEventListener('click', () => {
+            document
+              .getElementsByClassName(alpha)[0]
+              .scrollIntoView({
+                block: "center"
+              });
+          });
+        })
+      })
   }
 
   render() {
     if (this.props.breweries.length < 49) return null;
     return (
       <section className="brewery-index">
+        <div className="brewery-nav">
+          {alphabet.map((alpha, i) => (
+            <li key={i} id={alpha}>
+              {alpha[0]}
+            </li>
+          ))}
+        </div>
         <div className="brewery-index-list">
           {this.props.breweries.map((brewery, i) => (
-            <div key={i} className="brewery">
+            <div key={i} className={`brewery ${brewery.name[0]}`}>
               <Brewery
                 breweryId={brewery.id}
                 brewery={brewery}
@@ -21,13 +40,14 @@ class BreweryIndex extends React.Component {
               />
             </div>
           ))}
+          <div className="Z"></div>
         </div>
 
         <div className="beer-diagram-container">
-          <img src="./craftbeer.png" className="beer-diagram"/>
+          <img src="./craftbeer.png" className="beer-diagram" />
         </div>
       </section>
-    )
+    );
   }
 }
 
