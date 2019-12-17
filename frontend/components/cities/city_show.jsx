5cf3d9a8ab3e65dba873cc35f278d79244d0dfbe
Joanna Chen <x.chenj@gmail.com>
2019-12-17 00:54:56 -0800

    Style city show

--- frontend/components/cities/city_show.jsx
+++ frontend/components/cities/city_show.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Brewery from '../breweries/brewery_index_item';
 import GuidePreview from '../guides/guide_index_item';
 
 class CityShow extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
   componentDidMount() {
     this.props.fetchCity(this.props.match.params.cityId);
   }
@@ -19,28 +15,38 @@ class CityShow extends React.Component {
 
     const { breweries, guides, city } = this.props;
     
-    const imgUrl = city.photoUrl
-    const divStyle = {
-      backgroundImage: `url('${imgUrl}')`,
-    };
+    // const imgUrl = city.photoUrl
+    // const divStyle = {
+    //   backgroundImage: `url('${imgUrl}')`,
+    // };
 
     return (
-      <section className="city-show">
-        <h1>{city.name}</h1>
-        
-        <div>
-          {guides.map((guide, i) => (
-            <GuidePreview guide={guide} key={i} />
+      <div className="city-show">
+        <div className="guide city-guides">
+          <h1>{city.name} Guides</h1>
+          {guides.map(guide => (
+            <Link to={`/guides/${guide.id}`}>
+              <h1>{guide.title}</h1>
+              <h2>{guide.author}</h2>
+            </Link>
           ))}
         </div>
 
-        <div className="guide">
-          {breweries.map((brewery, i) => (
-            <Brewery brewery={brewery} key={i} breweries={breweries}/>
+        <div className="guide city-breweries">
+          {breweries.map(brewery => (
+            <>
+              <h1>
+                <a href={brewery.website} target="_blank">
+                  {brewery.name}
+                </a>
+              </h1>
+              <h2>{brewery.address}</h2>
+              <img src={brewery.photoUrl} className="brewery-photo" />
+            </>
           ))}
         </div>
-      </section>
-    )
+      </div>
+    );
   }
 }
 
