0ea442148706865d22bf30f39da934fdb755d45e
Joanna Chen <x.chenj@gmail.com>
2020-01-15 14:39:26 -0800

    Initialize post controller

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,39 @@
+class Api::PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render '/api/posts/index'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render '/api/posts/show'
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(guide_params)
+    if @post.save
+      render '/api/posts/posts'
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      render '/api/posts/posts'
+    else
+      render json: ['Post not found.'], status: 404
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
+end
