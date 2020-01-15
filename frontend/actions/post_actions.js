8557da719187805c8b5a03e2609a3c32cc959b79
Joanna Chen <x.chenj@gmail.com>
2020-01-15 14:53:13 -0800

    Initialize post actions

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,38 @@
+import * as PostAPIUtil from '../util/post_api_util';
+
+export const RECEIVE_ALL_POSTS = "RECEIVE_ALL_POSTS";
+export const RECEIVE_POST = "RECEIVE_POST";
+export const REMOVE_POST = "REMOVE_POST";
+
+const receivePosts = (posts) => ({
+  type: RECEIVE_ALL_POSTS,
+  posts
+})
+const receivePost = (post) => ({
+  type: RECEIVE_POST,
+  post
+})
+const removePost = (postId) => ({
+  type: REMOVE_POST,
+  postId
+})
+
+export const fetchPosts = () => dispatch => (
+  PostAPIUtil.fetchPosts()
+    .then(posts => dispatch(receivePosts(posts)))
+);
+
+export const fetchPost = (postId) => dispatch => (
+  PostAPIUtil.fetchPost(postId)
+    .then(post => dispatch(receivePost(post)))
+);
+
+export const createPost = (post) => dispatch => (
+  PostAPIUtil.createPost(post)
+    .then(post => dispatch(receivePost(post)))
+);
+
+export const deletePost = (postId) => dispatch => (
+  PostAPIUtil.deletePost(postId)
+    .then(res => dispatch(removePost(postId)))
+);
