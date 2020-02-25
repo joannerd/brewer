8561d25cc62b134a4bbb60b36fc7dbffbcea13d0
Joanna Chen <x.chenj@gmail.com>
2020-02-25 16:48:40 -0600

    Fix brewery item link render

--- frontend/components/breweries/brewery_index_item.jsx
+++ frontend/components/breweries/brewery_index_item.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 const Brewery = ({ brewery }) => {
   const addressLink = brewery.address.split(" ").join("+");
-  let websitePrint = brewery.website.replace("http://www.", "").replace("https://www.", "").replace("/", "");
 
   return (
     <>
@@ -11,10 +10,6 @@ const Brewery = ({ brewery }) => {
         <Link to={`/breweries/${brewery.id}`}>{brewery.name}</Link>
       </h1>
       <h2>
-        <a href={brewery.website} target="_blank">
-          {websitePrint}
-        </a>
-        <br/>
         {brewery.address}
       </h2>
       <img src={brewery.photoUrl} className="brewery-photo" />
@@ -25,6 +20,10 @@ const Brewery = ({ brewery }) => {
         target="_blank"
       >
         <button className="gmap-link">Open in Google Maps</button>
+      </a>
+      <br />
+      <a href={brewery.website} target="_blank">
+        <button className="gmap-link">Website</button>
       </a>
     </>
   );
