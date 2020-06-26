dc4cf57c2695b7d738a0c671e52032678c724979
Joanna Chen <x.chenj@gmail.com>
2020-06-25 23:03:38 -0700

    Refactor session form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,147 +1,145 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-class SessionForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      email: ""
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleDemo = this.handleDemo.bind(this);
-    this.update = this.update.bind(this);
-  }
+const SessionForm = ({
+  clearErrors,
+  processForm,
+  history,
+  formType,
+  errors,
+  formHeader,
+}) => {
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+    email: '',
+  });
 
-  componentDidMount() {
-    this.props.clearErrors();
-  }
+  useEffect(() => {
+    clearErrors();
+  }, [history]);
 
-  handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const user = Object.assign({}, this.state);
-    this.props.processForm(user)
-      .then(() => this.props.history.push("/"));
-  }
+    await processForm(user);
+    history.push('/');
+  };
 
-  handleDemo(e) {
+  const handleDemo = (e) => {
     e.preventDefault();
 
     let i = 0;
     let j = 0;
-    let demoUsername = "DemoUser";
-    let demoPassword = "123456";
+    const demoUsername = 'DemoUser';
+    const demoPassword = '123456';
 
     const typeUser = () => {
       let timeout;
       if (i < demoUsername.length) {
-        document.getElementById("username").value += demoUsername.charAt(i);
-        i++;
+        document.getElementById('username').value += demoUsername.charAt(i);
+        i += 1;
         timeout = setTimeout(typeUser, 100);
       } else {
         clearTimeout(timeout);
       }
-    }
+    };
 
     const typePw = () => {
       let timeout;
       if (j < demoPassword.length) {
-        document.getElementById("password").value += demoPassword.charAt(j);
-        j++;
+        document.getElementById('password').value += demoPassword.charAt(j);
+        j += 1;
         timeout = setTimeout(typePw, 100);
       } else {
         clearTimeout(timeout);
       }
-    }
+    };
 
-    if (this.props.formType === 'login') {
+    if (formType === 'login') {
       typeUser();
-      window.setTimeout(() => typePw(), 1000)
-      
+      window.setTimeout(() => typePw(), 1000);
       window.setTimeout(() => {
-        this.setState({ username: "DemoUser", password: "123456" }, () => {
-          const user = Object.assign({}, this.state);
-          this.props.processForm(user)
-            .then(() => this.props.history.push("/"));
-        });
-      }, 1600)
+        const demoUser = {
+          username: 'DemoUser',
+          password: '123456',
+        };
+        processForm(demoUser)
+          .then(() => history.push('/'));
+      }, 1600);
     }
-  }
+  };
 
-  update(field) {
-    return e => this.setState({[field]: e.target.value});
-  }
+  const update = (field) => e => setUser({ [field]: e.target.value });
 
-  render() {
-    return (
-      <div className="form-container flex-center">
-        <span className="logo">BREWER</span>        
+  return (
+    <div className="form-container flex-center">
+      <span className="logo">BREWER</span>
 
-        <div className="links">
-          <Link to="/login" className="session-link"><span className={this.props.formType === "login" ? "active" : "inactive"}>LOG IN</span>
-          </Link>
+      <div className="links">
+        <Link to="/login" className="session-link">
+          <span className={formType === 'login' ? 'active' : 'inactive'}>LOG IN</span>
+        </Link>
 
-          <Link to="/signup" className="session-link"><span className={this.props.formType === "signup" ? "active" : "inactive"}>SIGN UP</span>
-          </Link>
-        </div>
-        <br/>
-
-        <div>
-          { this.props.errors.map((err, i) => (
-            <div className="user-auth-errors" key={i}>
-              {err}
-            </div>
-          ))}
-        </div>
-
-        <form className="form flex-center">
-          <input
-            id="username"
-            className="input" 
-            onChange={this.update("username")}
-            placeholder="Username"
-            type="text"
-            name="username"
-            value={this.state.username}
-          />
-
-          <input
-            id="password"
-            className="input"
-            onChange={this.update("password")}
-            placeholder="Password"
-            type="password"
-            name="password"
-            value={this.state.password}
-          />
-
-          <input
-            className={this.props.formType === "signup" ? "input email" : "hidden"}
-            onChange={this.update("email")}
-            placeholder="Email"
-            type="text"
-            name="email" 
-            value={this.state.email}
-          />
-
-          <input
-            className="input submit"
-            onClick={this.handleSubmit}
-            type="submit"
-            value={this.props.formHeader}
-          />
-
-          <input
-            className={this.props.formType === "login" ? "input submit" : "hidden"}
-            onClick={this.handleDemo}
-            type="submit"
-            value="Demo Login"
-          />
-        </form>
+        <Link to="/signup" className="session-link">
+          <span className={formType === 'signup' ? 'active' : 'inactive'}>SIGN UP</span>
+        </Link>
       </div>
-    );
-  }
-}
+      <br />
+
+      <div>
+        { errors.map((err, i) => (
+          <div className="user-auth-errors" key={i}>
+            {err}
+          </div>
+        ))}
+      </div>
+
+      <form className="form flex-center">
+        <input
+          id="username"
+          className="input" 
+          onChange={update("username")}
+          placeholder="Username"
+          type="text"
+          name="username"
+          value={user.username}
+        />
+
+        <input
+          id="password"
+          className="input"
+          onChange={update('password')}
+          placeholder="Password"
+          type="password"
+          name="password"
+          value={user.password}
+        />
+
+        <input
+          className={formType === 'signup' ? 'input email' : 'hidden'}
+          onChange={update('email')}
+          placeholder="Email"
+          type="text"
+          name="email" 
+          value={user.email}
+        />
+
+        <input
+          className="input submit"
+          onClick={handleSubmit}
+          type="submit"
+          value={formHeader}
+        />
+
+        <input
+          className={formType === 'login' ? 'input submit' : 'hidden'}
+          onClick={handleDemo}
+          type="submit"
+          value="Demo Login"
+        />
+      </form>
+    </div>
+  );
+};
 
 export default SessionForm;
