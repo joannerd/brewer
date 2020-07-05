50a2f54778f2bcf96b89a375c9470deee0ec3c12
Joanna Chen <x.chenj@gmail.com>
2020-07-05 16:29:18 -0700

    Add error handling for guide brewery input

--- app/controllers/api/guides_controller.rb
+++ app/controllers/api/guides_controller.rb
@@ -17,13 +17,19 @@ class Api::GuidesController < ApplicationController
 
   def create
     @guide = Guide.new(guide_params)
-    if @guide.save
-      params[:guide][:breweries].each do | order, brewery_id |
-        BreweryGuide.create({brewery_id: brewery_id.to_i, guide_id: @guide.id, order: order})
-      end
-      render '/api/guides/show'
+    errors = []
+    if !params[:guide][:breweries]
+      errors.push('Guide must have at least one brewery')
+      render json: errors, status: 422
     else
-      render json: @guide.errors.full_messages, status: 422
+      if @guide.save
+        params[:guide][:breweries].each do | order, brewery_id |
+          BreweryGuide.create({brewery_id: brewery_id.to_i, guide_id: @guide.id, order: order})
+        end
+        render '/api/guides/show'
+      else
+        render json: @guide.errors.full_messages, status: 422
+      end
     end
   end
 
